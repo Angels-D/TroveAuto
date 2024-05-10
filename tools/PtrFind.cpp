2830482f94eb578dc1a7d44f0d76ec07e2b57aef
Angels-D <d1005562341@126.com>
2024-05-10 19:35:13 +0800

    添加指针本地扫描，程序单文件打包

--- tools/PtrFind.cpp
+++ tools/PtrFind.cpp
@@ -1,40 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <Windows.h>
 #include <TlHelp32.h>
-
-//big signatures for best guarantee (i just selected random length))
-
-std::vector<int> player_offset_signature =
-{
-    0x55, 0x8B, 0xEC, 0x83, 0xE4, 0xF8, 0x83, 0xEC, 0x08, 0xF3, 0x0F, 0x2A, 0x45, 0x10, 0x56, 0x8B, 0xF1,
-    0x57, 0x8B, 0x3D
-};
-
-std::vector<int> world_offset_signature =
-{
-    0x55, 0x8B, 0xEC, 0x83, 0x7D, 0x08, 0x04, 0x75, 0x10, 0xA1, -1, -1, -1, -1, 0x85, 0xC0, 0x74, 0x07,
-    0xC6, 0x80, 0x59, 0x01, 0x00, 0x00, 0x01, 0x5D, 0xC2, 0x04, 0x00
-};
-
-std::vector<int> settings_offset_signature =
-{
-    0x89, 0x45, 0xF4, 0x8B, 0x11, 0xFF, 0x52, 0x0C, 0x8B, 0x0D, -1, -1, -1, -1, 0x8B, 0xD8,
-    0x6A, 0x03, 0x68, -1, -1, -1, -1, 0x8B, 0x11, 0xFF, 0x52, 0x0C, 0x8B, 0x0D
-};
-
-std::vector<int> chat_state_offset_signature =
-{
-    0x8B, 0x0D, -1, -1, -1, -1, 0x6A, 0x00, 0x6A, 0x01, 0xC6, 0x41, 0x20, 0x00, 0xE8, -1,
-    -1, -1, -1, 0x6A, 0x08, 0x8D, 0x8D, 0x60, 0xFF, 0xFF, 0xFF, 0xC7, 0x85, 0x60, 0xFF, 0xFF, 0xFF,
-    0x00, 0x00, 0x00, 0x00, 0xC7, 0x85, 0x64, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xC7, 0x85, 0x68, 0xFF,
-    0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xC6, 0x45, 0xF0, 0x01
-};
 
 HANDLE process_handle = 0;
 int process_id = 0;
 MODULEENTRY32W process_module;
 UINT32 module_address = 0;
+
+/* 地址寻址
+ * 玩家: (+Size Value) 55 8B EC 83 E4 F8 83 EC 08 F3 0F 2A 45 10 56 8B F1 57 8B 3D
+ * 世界: (+10   Value) 55 8B EC 83 7D 08 04 75 10 A1 XX XX XX XX 85 C0 74 07 C6 80 59 01 00 00 01 5D C2 04 00
+ * 设置: (+Size Value) 89 45 F4 8B 11 FF 52 0C 8B 0D XX XX XX XX 8B D8 6A 03 68 XX XX XX XX 8B 11 FF 52 0C 8B 0D
+ * 聊天: (+2    Value) 8B 0D XX XX XX XX 6A 00 6A 01 C6 41 20 00 E8 XX XX XX XX 6A 08 8D 8D 60 FF FF FF C7 85 60 FF FF FF 00 00 00 00 C7 85 64 FF FF FF 00 00 00 00 C7 85 68 FF FF FF 00 00 00 00 C6 45 F0 01
+ * 隐藏特效: (+3 Address) F3 0F 11 44 24 24 F3 0F 58 84 24 80 00 00 00 50 F3 0F 11 43 24 E8 XX XX XX XX 8D 44 24 34 50
+ * 自动攻击: (+1 Address) DF F1 DD D8 72 1F
+ * 打破障碍: (+3 Address) 80 7F XX 00 0F 84 XX XX XX XX 8B 4B 08 E8 XX XX XX XX FF 75 0C 8B 4D 10 8B F0 FF 75 08 8B 45 14 83 EC 0C 8B 3E 8B D4 6A 01 89 0A 8B CE 89 42 04 8B 45 18
+ * 视角遮挡: (+0 Address) 0F 29 01 C7 41 34 00 00 00 00 0F
+ * 保持骑乘: (+0 Address) 74 XX 8B 07 8B CF 6A 00 6A 00 FF 50
+ * 钓鱼地址: (+0 Address) 10 14 XX XX 00 00 00 00 FF 00 00 00 00
+ * 视角固定: (+0 Address) 74 05 8B 01 FF 50 0C 8B E5
+ * 地图放大: (+0 Address) 77 XX B8 XX XX XX XX F3 0F 10 08 F3 0F 11 89 XX XX XX XX 8B 89
+ * 快速挖矿: (+1 Address) DF F1 DD D8 72 61
+ * 快速挖矿（晶洞）: (+1 Address) DF F1 DD D8 72 35 8D
+ * 账号地址: (+6 Address) E8 XX XX XX FF A1 XX XX XX 01 68 XX XX XX 01 FF
+ *           00 00 00 00 E8 X9 XX XX FF A1 XX 6X XX 01 68 XX XX XX 01 FF 70 1C FF 70 18 8D 45 B0 50 E8 X0 7X XX FF 83 C4 10 8D 45 B0
+ *           75 06 8B 01 6A 01 FF 10 C7 05 XX XX XX 01 00 00 00 00 C3 CC CC CC 8B 0D XX 6X XX 01 85 C9 74 16 83 41 08 FF 75 06 8B 01
+ * 视野放大: (+3 Address) F3 0F 11 5F 2C
+ */
 
 void get_process_id(const wchar_t* process_name)
 {
@@ -96,8 +90,6 @@ int find_signature_address(std::vector<int> pattern)
     DWORD module_size = process_module.modBaseSize;
     auto buffer = (byte*)malloc(module_size);
     ReadProcessMemory(process_handle, (LPCVOID)module_address, buffer, module_size, 0);
-    //ReadProcessMemory(process_handle, module_address, buffer);
-    //ReadProcessMemory(process.Handle, moduleAddress, buffer, moduleSize, out _);
 
     int compare = 0;
     UINT32 count = pattern.size();
@@ -128,40 +120,15 @@ int find_signature_address(std::vector<int> pattern)
     return 0;
 }
 
-int main()
+int main(int argc,char *argv[])
 {
-    SetConsoleTitle(L"Offsets finder");
     get_process_id(L"Trove.exe");
-    while (process_id == 0)
-    {
-        printf("process not found\n");
-        Sleep(1000);
-        get_process_id(L"Trove.exe");
-    }
-    printf("process found: %X (%i)\n\n", process_id, process_id);
     get_process_module(L"Trove.exe");
     process_handle = OpenProcess(PROCESS_ALL_ACCESS, NULL, process_id);
-
-    UINT32 local_player_offset, world_offset, settings_offset, chat_state_offset;
-
-    UINT32 address = find_signature_address(player_offset_signature) + player_offset_signature.size();
-    address = read_int(address);
-    local_player_offset = address - module_address;
-
-    address = find_signature_address(world_offset_signature) + 10;
-    address = read_int(address);
-    world_offset = address - module_address;
-
-    address = find_signature_address(settings_offset_signature) + settings_offset_signature.size();
-    address = read_int(address);
-    settings_offset = address - module_address;
-
-    address = find_signature_address(chat_state_offset_signature) + 2;
-    address = read_int(address);
-    chat_state_offset = address - module_address;
-
-    printf("Local Player offset: %X\nWorld offset: %X\nSettings offset: %X\nChat State offset: %X\n", local_player_offset, world_offset, settings_offset, chat_state_offset);
-
-    getchar();
+    std::vector<int> offset_signature;
+    char * str;
+    for(int i = 1; argv[i]; i++)
+        offset_signature.push_back(strcmp(argv[i],"XX")?strtol(argv[i],&str,16):-1);
+    printf("0x%0X",find_signature_address(offset_signature) - module_address);
     return 0;
 }
